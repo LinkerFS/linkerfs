f5e88dfa4b83054a779681aab2896bc265f83af6
kaedeair <34467508+kaedeair@users.noreply.github.com>
2024-04-10 19:09:22 +0800

    feat(data): add data structure of every part info

---
+++ src/data/partinfo.h
@@ -0,0 +1,26 @@
+/*
+  LinkerFS: Filesystem based on FUSE for splitting or unioning streams
+  Copyright (C) 2024  kaedeair <kaedeair@outlook.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU LGPLv3 License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the file LICENSE for more details.
+*/
+
+#ifndef LINKERFS_PATHINFO_H
+#define LINKERFS_PATHINFO_H
+
+#include <stdint.h>
+
+
+struct PathInfo {
+    uint64_t fileSize;
+    uint32_t pathOffset;
+    uint16_t pathLength;
+};
+
+#endif //LINKERFS_PATHINFO_H
