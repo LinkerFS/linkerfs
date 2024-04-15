22627760e5cc4a242c933c9e0e62d96a53c615f7
kaedeair <34467508+kaedeair@users.noreply.github.com>
2024-04-15 21:06:03 +0800

    chore: change project description

--- src/linkerfs.c
+++ src/linkerfs.c
@@ -1,5 +1,5 @@
 /*
-  LinkerFS: Filesystem based on FUSE for splitting or unioning streams
+  LinkerFS: a data remapping filesystem
   Copyright (C) 2024  kaedeair <kaedeair@outlook.com>
 
   This program is free software: you can redistribute it and/or modify
