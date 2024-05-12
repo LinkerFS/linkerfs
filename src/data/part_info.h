170eb58093956b86b81d33a3ecdc2f1d3ba8f332
kaedair <34467508+kaedeair@users.noreply.github.com>
2024-05-12 14:47:13 +0800

    chore: Update Copyright

--- src/data/part_info.h
+++ src/data/part_info.h
@@ -1,15 +1,23 @@
 /*
-  LinkerFS: a data remapping filesystem
-  Copyright (C) 2024  kaedeair <kaedeair@outlook.com>
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU LGPLv3 License.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the file LICENSE for more details.
-*/
+ * LinkerFS: a data remapping filesystem
+ * Copyright (C) 2024  kaedeair <kaedeair@outlook.com>
+ *
+ * This file is part of LinkerFS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LinkerFS. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef LINKERFS_PART_INFO_H
 #define LINKERFS_PART_INFO_H
